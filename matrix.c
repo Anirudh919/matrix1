53056802e02e7f85b093155231abea96b7143188
Anirudh919 <101653588+Anirudh919@users.noreply.github.com>
2022-05-22 17:05:09 +0530

    commit

---
+++ matrix.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+void addmatrix(int a[][])
+{
+    int sum, m, n, i, j;
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            sum = a[i][j] + (++a[i][j]);
+        }
+    }
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            printf("Addition of matrix%d:\n", sum);
+        }
+    }
+}
+void subtractmatrix()
+{
+}
+void multiplicationmatrix()
+{
+}
+
+int main()
+{
+    int n, i, j, b, a[10][10], m, c, choice, d;
+    printf("Enter the how many number of matrix insert:");
+    scanf("%d", &c);
+    for (b = 0; b < c; b++)
+    {
+        printf("Enter the number of column & row of matrix:");
+        scanf("%d%d", &m, &n);
+        printf("Enter the matrix %d:", b + 1);
+        for (i = 0; i < m; i++)
+        {
+            for (j = 0; j < n; j++)
+            {
+                scanf("%d", &a[i][j]);
+            }
+        }
+        printf("Matrix %d:\n", b + 1);
+        for (i = 0; i < m; i++)
+        {
+            for (j = 0; j < n; j++)
+            {
+                printf("%d\t", a[i][j]);
+            }
+            printf("\n");
+        }
+    }
+    do
+    {
+        printf("\n1. Addition of matrix\n2. Subtraction of matrix\n3. Multiplication of matrix\n4. Exit");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            addmatrix(a[i][j]);
+            break;
+        case 2:
+            subtractmatrix();
+            break;
+        case 3:
+            multiplicationmatrix();
+            break;
+        case 4:
+            exit(0);
+            break;
+
+        default:
+            printf("Enter the valid choice:");
+            break;
+        }
+    } while (d == 'y');
+}
